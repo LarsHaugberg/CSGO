4c1e38aed07c8d1d38d36509e43f2262bc0a37d1
LarsHaugberg <110969311+LarsHaugberg@users.noreply.github.com>
2023-04-26 15:17:23 +0200

    R

--- PP med Lars/Program.cs
+++ PP med Lars/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace PP_med_Lars
+﻿using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace PP_med_Lars
 {
     internal class Program
     {
         static void Main()
         {
-            var game;
+            
+
         }
 
     
